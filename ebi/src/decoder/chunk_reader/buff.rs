3a0a03d461994da8908e2a76cd2368a85605b850
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-07-30 11:57:51 -0500

    Clean up buff_simd256_decode

--- ebi/src/decoder/chunk_reader/buff.rs
+++ ebi/src/decoder/chunk_reader/buff.rs
@@ -46,8 +46,7 @@ impl<R: Read> Reader for BUFFReader<R> {
         let mut buf = vec![0; self.chunk_size as usize];
         self.reader.read_exact(&mut buf)?;
 
-        // TODO: use the scale from the header
-        let result = internal::buff_simd256_decode(100, buf);
+        let result = internal::buff_simd256_decode(buf);
 
         self.decompressed = Some(result);
 
@@ -87,118 +86,70 @@ impl<R: Read> QueryExecutor for BUFFReader<R> {}
 mod internal {
     use std::mem;
 
-    use crate::compression_common::buff::{
-        bit_packing::BitPack, flip, get_precision_bound, precision_bound::PrecisionBound,
-    };
+    use crate::compression_common::buff::{bit_packing::BitPack, flip};
 
-    pub fn buff_simd256_decode(scale: usize, bytes: Vec<u8>) -> Vec<f64> {
-        let precision = (scale as f32).log10() as i32;
-
+    pub fn buff_simd256_decode(bytes: Vec<u8>) -> Vec<f64> {
         let mut bitpack = BitPack::<&[u8]>::new(bytes.as_slice());
         let lower = bitpack.read(32).unwrap();
         let higher = bitpack.read(32).unwrap();
-        let ubase_int = (lower as u64) | ((higher as u64) << 32);
-        let base_int = unsafe { mem::transmute::<u64, i64>(ubase_int) };
-        println!("base integer: {}", base_int);
-        let len = bitpack.read(32).unwrap();
-        println!("total vector size:{}", len);
+        let base_fixed64_bits = (lower as u64) | ((higher as u64) << 32);
+        let base_fixed64 = unsafe { mem::transmute::<u64, i64>(base_fixed64_bits) };
+
+        let number_of_records = bitpack.read(32).unwrap();
+
         let fixed_representation_bits_length = bitpack.read(32).unwrap();
-        println!(
-            "fixed_representation_bits_length:{}",
-            fixed_representation_bits_length
-        );
-        let dlen = bitpack.read(32).unwrap();
-        // check integer part and update bitmap;
 
-        let mut expected_datapoints: Vec<f64> = Vec::new();
-        let mut fixed_vec: Vec<u64> = Vec::new();
+        let fractional_part_bits_length = bitpack.read(32).unwrap();
 
-        let mut dec_scl: f64 = 2.0f64.powi(dlen as i32);
-        println!("Scale for decimal:{}", dec_scl);
+        let scale: f64 = 2.0f64.powi(fractional_part_bits_length as i32);
 
-        let mut remain = fixed_representation_bits_length;
-        let mut bytec = 0;
-        let mut chunk;
-        let mut f_cur = 0f64;
-        let mut cur = 0;
+        let mut remaining_bits_length = fixed_representation_bits_length;
 
-        if remain < 8 {
-            for i in 0..len {
-                cur = bitpack.read_bits(remain as usize).unwrap();
-                expected_datapoints.push((base_int + cur as i64) as f64 / dec_scl);
+        let expected_datapoints: Vec<f64> = if remaining_bits_length < 8 {
+            let mut expected_datapoints = Vec::with_capacity(number_of_records as usize);
+            for _ in 0..number_of_records {
+                let cur = bitpack.read_bits(remaining_bits_length as usize).unwrap();
+                expected_datapoints.push((base_fixed64 + cur as i64) as f64 / scale);
             }
-            remain = 0
+
+            expected_datapoints
         } else {
-            bytec += 1;
-            remain -= 8;
-            chunk = bitpack.read_n_byte(len as usize).unwrap();
+            let mut fixed_vec: Vec<u64> = Vec::with_capacity(number_of_records as usize);
+            remaining_bits_length -= 8;
+            let subcolumn_chunk = bitpack.read_n_byte(number_of_records as usize).unwrap();
 
-            if remain == 0 {
-                for &x in chunk {
-                    expected_datapoints.push((base_int + flip(x) as i64) as f64 / dec_scl);
+            for x in subcolumn_chunk {
+                fixed_vec.push((flip(*x) as u64) << remaining_bits_length)
+            }
+
+            while remaining_bits_length >= 8 {
+                remaining_bits_length -= 8;
+                let subcolumn_chunk = bitpack.read_n_byte(number_of_records as usize).unwrap();
+
+                for (fixed, chunk) in fixed_vec.iter_mut().zip(subcolumn_chunk.iter()) {
+                    *fixed |= (flip(*chunk) as u64) << remaining_bits_length;
                 }
+            }
+
+            if remaining_bits_length > 0 {
+                let last_subcolumn_chunk = (0..number_of_records)
+                    .map(|_| bitpack.read_bits(remaining_bits_length as usize).unwrap() as u64);
+
+                fixed_vec
+                    .into_iter()
+                    .zip(last_subcolumn_chunk)
+                    .map(|(fixed, last_subcolumn)| {
+                        let delta_fixed = fixed | last_subcolumn;
+                        (base_fixed64 + delta_fixed as i64) as f64 / scale
+                    })
+                    .collect()
             } else {
-                // dec_vec.push((bitpack.read_byte().unwrap() as u32) << remain);
-                // let mut k = 0;
-                for x in chunk {
-                    // if k<10{
-                    //     println!("write {}th value with first byte {}",k,(*x))
-                    // }
-                    // k+=1;
-                    fixed_vec.push((flip(*x) as u64) << remain)
-                }
+                fixed_vec
+                    .into_iter()
+                    .map(|x| (base_fixed64 + x as i64) as f64 / scale)
+                    .collect()
             }
-            println!("read the {}th byte of dec", bytec);
-
-            while (remain >= 8) {
-                bytec += 1;
-                remain -= 8;
-                chunk = bitpack.read_n_byte(len as usize).unwrap();
-                if remain == 0 {
-                    // dec_vec=dec_vec.into_iter().map(|x| x|(bitpack.read_byte().unwrap() as u32)).collect();
-                    // let mut iiter = int_vec.iter();
-                    // let mut diter = dec_vec.iter();
-                    // for cur_chunk in chunk.iter(){
-                    //     expected_datapoints.push( *(iiter.next().unwrap()) as f64+ (((diter.next().unwrap())|((*cur_chunk) as u32)) as f64) / dec_scl);
-                    // }
-
-                    for (cur_fixed, cur_chunk) in fixed_vec.iter().zip(chunk.iter()) {
-                        expected_datapoints.push(
-                            (base_int + ((*cur_fixed) | (flip(*cur_chunk) as u64)) as i64) as f64
-                                / dec_scl,
-                        );
-                    }
-                } else {
-                    let mut it = chunk.into_iter();
-                    fixed_vec = fixed_vec
-                        .into_iter()
-                        .map(|x| x | ((flip(*(it.next().unwrap())) as u64) << remain))
-                        .collect();
-                }
-
-                println!("read the {}th byte of dec", bytec);
-            }
-            // let duration = start.elapsed();
-            // println!("Time elapsed in leading bytes: {:?}", duration);
-
-            // let start5 = Instant::now();
-            if (remain > 0) {
-                bitpack.finish_read_byte();
-                println!("read remaining {} bits of dec", remain);
-                println!("length for fixed:{}", fixed_vec.len());
-                for cur_fixed in fixed_vec.into_iter() {
-                    f_cur = (base_int
-                        + ((cur_fixed) | (bitpack.read_bits(remain as usize).unwrap() as u64))
-                            as i64) as f64
-                        / dec_scl;
-                    expected_datapoints.push(f_cur);
-                }
-            }
-        }
-        // for i in 0..10{
-        //     println!("{}th item:{}",i,expected_datapoints.get(i).unwrap())
-        // }
-        println!("Number of scan items:{}", expected_datapoints.len());
+        };
         expected_datapoints
     }
 }
