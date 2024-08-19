38b4e3b5a25a911942456f16fa40f72f8a843895
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-08-19 02:36:46 -0500

    Impl FFIAlpReader

--- alp-binding/src/lib.rs
+++ alp-binding/src/lib.rs
@@ -21,12 +21,20 @@ pub type ExpCT = u16;
 pub const VECTOR_SIZE: usize = 1024;
 
 pub fn worst_case_compression_size_double(n_tuple: usize) -> usize {
-    let n_vector = n_tuple.next_multiple_of(VECTOR_SIZE) / VECTOR_SIZE;
+    let n_tuple_aligned = n_tuple.next_multiple_of(VECTOR_SIZE);
+    let n_vector = n_tuple_aligned / VECTOR_SIZE;
+
     // extra space for exception position and center
     let if_all_exception = n_vector * VECTOR_SIZE * size_of::<f64>()
         + size_of::<ExpPT>()
         + n_vector * size_of::<ExpCT>();
-    n_tuple * size_of::<f64>() + if_all_exception
+    let size = n_tuple_aligned * size_of::<f64>() + if_all_exception;
+
+    if n_tuple < VECTOR_SIZE {
+        size * 2
+    } else {
+        size
+    }
 }
 
 /// Compresses a slice of `f64` values and stores the result in the provided buffer.
@@ -36,6 +44,7 @@ pub fn worst_case_compression_size_double(n_tuple: usize) -> usize {
 /// * `input` - A slice of `f64` values to be compressed.
 /// * `buffer` - A mutable reference to a `Vec<u8>` where the compressed data will be stored.
 ///
+#[inline]
 pub fn compress_double(input: &[f64], buffer: &mut Vec<u8>) {
     moveit! {
         let mut compressor = AlpCompressorDouble::new();
@@ -49,7 +58,12 @@ pub fn compress_double(input: &[f64], buffer: &mut Vec<u8>) {
             .compress(input.as_ptr() as *mut f64, input.len(), buffer.as_mut_ptr());
     }
     let compressed_size = compressor.as_mut().get_size();
-    debug_assert!(compressed_size <= buf_size);
+    debug_assert!(
+        compressed_size <= buf_size,
+        "Compressed size exceeds buffer size: {} > {}",
+        compressed_size,
+        buf_size
+    );
     unsafe {
         buffer.set_len(compressed_size);
     }
@@ -65,6 +79,7 @@ pub fn compress_double(input: &[f64], buffer: &mut Vec<u8>) {
 ///
 /// # Safety
 /// * The number of records must be valid for the given compressed data.
+#[inline]
 pub unsafe fn decompress_double(input: &[u8], number_of_records: usize, buffer: &mut Vec<f64>) {
     moveit! {
         let mut decompressor = AlpDecompressorDouble::new();
---
+++ ebi/src/decoder/chunk_reader/ffi_alp.rs
@@ -0,0 +1,92 @@
+use std::{io::Read, iter, slice};
+
+use crate::decoder::{self, query::QueryExecutor, FileMetadataLike, GeneralChunkHandle};
+
+use super::Reader;
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct FFIAlpReader<R: Read> {
+    reader: R,
+    chunk_size: usize,
+    decompressed: Vec<f64>,
+    number_of_records: usize,
+    is_decompressed: bool,
+}
+
+impl<R: Read> FFIAlpReader<R> {
+    pub fn new<F: FileMetadataLike>(handle: &GeneralChunkHandle<F>, reader: R) -> Self {
+        let number_of_records = handle.number_of_records() as usize;
+        let chunk_size = handle.chunk_size() as usize;
+        Self {
+            reader,
+            chunk_size,
+            number_of_records,
+            decompressed: vec![0.0; number_of_records],
+            is_decompressed: false,
+        }
+    }
+}
+
+type F = fn(&f64) -> decoder::Result<f64>;
+pub type FFIAlpDecompressIterator<'a> = iter::Map<slice::Iter<'a, f64>, F>;
+impl<R: Read> Reader for FFIAlpReader<R> {
+    type NativeHeader = ();
+
+    type DecompressIterator<'a> = FFIAlpDecompressIterator<'a>
+    where
+        Self: 'a;
+
+    fn decompress(&mut self) -> decoder::Result<&[f64]> {
+        if self.is_decompressed {
+            return Ok(self.decompressed.as_slice());
+        }
+
+        let mut src_buf = vec![0; self.chunk_size];
+        self.reader.read_exact(&mut src_buf)?;
+
+        unsafe {
+            alp_binding::decompress_double(
+                src_buf.as_slice(),
+                self.number_of_records,
+                &mut self.decompressed,
+            );
+        }
+
+        self.is_decompressed = true;
+
+        Ok(self.decompressed.as_slice())
+    }
+
+    fn decompress_iter(&mut self) -> decoder::Result<Self::DecompressIterator<'_>> {
+        if !self.is_decompressed {
+            self.decompress()?;
+        }
+
+        Ok(self.decompressed.iter().map(|&x| Ok(x)))
+    }
+
+    fn set_decompress_result(&mut self, data: Vec<f64>) -> &[f64] {
+        self.decompressed = data;
+        self.is_decompressed = true;
+
+        self.decompressed.as_slice()
+    }
+
+    fn decompress_result(&mut self) -> Option<&[f64]> {
+        if self.is_decompressed {
+            Some(self.decompressed.as_slice())
+        } else {
+            None
+        }
+    }
+
+    fn header_size(&self) -> usize {
+        0
+    }
+
+    fn read_header(&mut self) -> &Self::NativeHeader {
+        &()
+    }
+}
+
+impl<R: Read> QueryExecutor for FFIAlpReader<R> {}
