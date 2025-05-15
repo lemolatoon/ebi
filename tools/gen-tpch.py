b2a0311675b394f183b2a0ac5e9895e721171610
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2025-05-15 15:16:08 +0900

    Use INSTALL/LOAD tpch; stmts directly in gen-tpch.py

--- tools/gen-tpch.py
+++ tools/gen-tpch.py
@@ -1,12 +1,13 @@
 import duckdb
-from duckdb_extensions import extension_importer
+# from duckdb_extensions import extension_importer
 
-# tpch 拡張をインポート
-extension_importer.import_extension("tpch")
+# extension_importer.import_extension("tpch")
 
 conn = duckdb.connect()
 
-# すぐに dbgen を呼べる
+conn.execute("INSTALL tpch; LOAD tpch;")
+print("tpch installed and loaded.")
+
 conn.execute("CALL dbgen(sf := 10);")
 
 # データ確認
